69e7b8dc02fad5605dec4c8f8fc439585d0d4e71
nursenem <220229025@kocaeli.edu.tr>
2025-07-15 18:14:14 -0400

    feat: PackageModel Update

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -1,6 +1,7 @@
 from pydantic import Field, validator
 from typing import List, Optional, Union, Literal
-from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, Config
+from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, \
+    Config
 
 
 class InputImage(Input):
@@ -22,7 +23,7 @@ class InputImage(Input):
 
 class OutputImage(Output):
     name: Literal["outputImage"] = "outputImage"
-    value: Union[List[Image],Image]
+    value: Union[List[Image], Image]
     type: str = "object"
 
     @validator("type", pre=True, always=True)
@@ -35,7 +36,6 @@ class OutputImage(Output):
 
     class Config:
         title = "Image"
-
 
 
 class KeepSideFalse(Config):
@@ -76,13 +76,55 @@ class Degree(Config):
         Positive angles specify counterclockwise rotation while negative angles indicate clockwise rotation.
     """
     name: Literal["Degree"] = "Degree"
-    value: int = Field(ge=-359.0, le=359.0,default=0)
+    value: int = Field(ge=-359.0, le=359.0, default=0)
     type: Literal["number"] = "number"
     field: Literal["textInput"] = "textInput"
     placeHolder: Literal["[-359, 359]"] = "[-359, 359]"
 
     class Config:
         title = "Angle"
+
+
+class CompareExecutorInputs(Inputs):
+    inputImage: InputImage
+
+
+class CompareExecutorConfigs(Configs):
+    drawBBox: KeepSideBBox
+
+
+class CompareExecutorOutputs(Outputs):
+    outputImage: OutputImage
+
+
+class CompareExecutorRequest(Request):
+    inputs: Optional[CompareExecutorInputs]
+    configs: CompareExecutorConfigs
+
+    class Config:
+        json_schema_extra = {
+            "target": "configs"
+        }
+
+
+class CompareExecutorResponse(Response):
+    outputs: CompareExecutorOutputs
+
+
+class CompareExecutor(Config):
+    name: Literal["CompareExecutor"] = "CropExecutor"
+    value: Union[CompareExecutorRequest, CompareExecutorResponse]
+    type: Literal["object"] = "object"
+    field: Literal["option"] = "option"
+
+    class Config:
+        title = "Package"
+        json_schema_extra = {
+            "target": {
+                "value": 0
+            }
+        }
+
 
 class CropExecutorInputs(Inputs):
     inputImage: InputImage
@@ -91,6 +133,7 @@ class CropExecutorInputs(Inputs):
 class CropExecutorConfigs(Configs):
     degree: Degree
     drawBBox: KeepSideBBox
+
 
 class CropExecutorOutputs(Outputs):
     outputImage: OutputImage
@@ -127,15 +170,12 @@ class CropExecutor(Config):
 
 class ConfigExecutor(Config):
     name: Literal["ConfigExecutor"] = "ConfigExecutor"
-    value: Union[CropExecutor]
+    value: Union[CropExecutor, CompareExecutor]
     type: Literal["executor"] = "executor"
     field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
 
     class Config:
         title = "Task"
-        json_schema_extra = {
-            "target": "value"
-        }
 
 
 class PackageConfigs(Configs):
